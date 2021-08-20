986b8afa2e241ce14dac8e8d9ebf70294ea02f85
Andric0901 <binseong.kim@mail.utoronto.ca>
2021-08-19 19:42:43 -0700

    Major changes! update to private channels + potential randomness

--- main.js
+++ main.js
@@ -65,21 +65,42 @@ client.on('messageCreate', async message => {
                 const sentMessage = await channel.send({ embeds: [embed] })
 
                 const collector = new MessageCollector(channel, m => m.author.id === message.author.id);
+                let userStopped = false;
+                const randomTitles = ['Random #1', 'Random #2', 'Random #3', 'Random #4']
+                const randomDescriptions = ['Description #1', 'Description #2', 'Description #3', 'Description #4']
+
                 collector.on('collect', msg => {
-                    if (msg.content.toLowerCase() == "y") {
+                    if (!msg.author.bot && !userStopped) {
+                        msg.delete().catch(console.error())
+                    }
+                    if (msg.content.toLowerCase() == "y" && !userStopped) {
+                        let randomInteger1 = getRndInteger(0, randomTitles.length)
+                        let randomInteger2 = getRndInteger(0, randomDescriptions.length)
                         embed = new MessageEmbed()
                             .setColor('#0099ff')
-                            .setTitle('Sample message?')
-                            .setDescription('Good morning!!! :)')
-                        msg.delete()
+                            .setTitle(randomTitles[randomInteger1])
+                            .setDescription(randomDescriptions[randomInteger2])
                         sentMessage.edit({ embeds: [embed] })
-                    } else if (msg.content.toLowerCase() == 'stop') {
-
+                    } else if (msg.content.toLowerCase() == 'stop' && !userStopped) {
+                        userStopped = !userStopped
+                        msg.channel.send('Thank you for using CareBot! I hope you have a wonderful day :)').then(endMessage => {
+                            setTimeout(() => {
+                                endMessage.delete().catch(console.error())
+                            }, 10000);
+                            setTimeout(() => {
+                                channel.delete().catch(console.error())
+                            }, 11000);
+                        })
                     }
                 })
             })
     }
 });
+
+// returns a random integer, min inclusive and max exclusive
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
 // Keep this at the end of the main.js file
 client.login(config.token);
